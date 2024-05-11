122e175e3ec26bc9290810ad2234c67c9eae8955
青木 宏賢 <aokihirotaka@aokinoMacBook-Pro.local>
2024-05-11 18:21:55 +0900

    feat: 🎸 set up APOLLO SERVER
    n the backend side, our first goal is to create a GraphQL server that
    can:
    
    BREAKING CHANGE: 🧨 exit

--- flow.md
+++ flow.md
@@ -1,3 +1,8 @@
+# tutorial url
+https://www.apollographql.com/tutorials/intro-typescript/03-schema-definition-language-sdl
+
 # 3
 Let's navigate to the src directory. In there, we're going to create a new file called schema.graphql.
 
+# 4
+`npm install @apollo/server graphql graphql-tag`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-// TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "fs";
+import path from "path";
+import { gql } from "graphql-tag";
+
+const typeDefs = gql(
+  // * 現在のディレクトリにあるschema.graphqlファイルを読み込み、その内容を文字列として取得する
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs }); // typeDefs: typeDefsのshorthand
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+  🚀  Server is running!
+  📭  Query at ${url}
+`);
+}
+
+startApolloServer();
